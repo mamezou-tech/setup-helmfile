99493a8b291e3b8c0e15986a36cdb8fbbe288a1e
kondoumh <kondoumh@gmail.com>
2020-02-03 00:27:16 +0900

    Set param

--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
 const core = require(`@actions/core`);
-const github = require(`@actions/github`);
 const {downloadHelmfile} = require("./setup");
 
 async function run() {
   try {
-    const kubeconfig = core.getInput('kubeconfig');
-    console.log(kubeconfig);
-    const helmfileVersion = "v0.98.2";
-  
+    const helmfileVersion = core.getInput("helmfile-version");
     downloadHelmfile(helmfileVersion);
-  
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`);
+
   } catch (error) {
     core.setFailed(error.message);
   }
