2b42ca0481a6e6e5844277df2d4258c8e868dec5
kondoumh <kondoumh@gmail.com>
2020-02-03 10:36:07 +0900

    install helm

--- src/setup.js
+++ src/setup.js
@@ -8,6 +8,7 @@ async function installHelm(version) {
   const downloadPath = await download(`https://get.helm.sh/helm-${version}-linux-amd64.tar.gz`, "helm");
   const folder = await extract(downloadPath);
   console.log(folder);
+  await install(`${folder}/linux-amd64/helm`, "helm");
 }
 
 async function installHelmfile(version) {
