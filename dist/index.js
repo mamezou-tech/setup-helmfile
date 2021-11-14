d96ae4a327064e00decee07509444920f4c32e61
kondoumh <kondoumh@gmail.com>
2021-11-14 09:37:54 +0900

    update package

--- dist/index.js
+++ dist/index.js
@@ -4869,8 +4869,8 @@ async function run() {
     }
     if (core.getInput("install-helm-plugins") === "yes") {
       installHelmPlugins([
-        'https://github.com/databus23/helm-diff --version master',
-        'https://github.com/hypnoglow/helm-s3.git',
+        'https://github.com/databus23/helm-diff --version ' + core.getInput("helm-diff-plugin-version"),
+        'https://github.com/hypnoglow/helm-s3.git --version ' + core.getInput("helm-s3-plugin-version"),
       ]);
     }
     const additionalPlugins = core.getInput("additional-helm-plugins")
